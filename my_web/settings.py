c87c8caf52978ff9b42aef394ee8175885ccda8e
RwtKuldeep <136052966+RwtKuldeep@users.noreply.github.com>
2024-07-23 18:27:35 +0530

    Update settings.py

--- my_web/settings.py
+++ my_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d@1zcz6#-1t-l@*@)^653+aq&!dgvllw7202g8!1uzpu77(3g-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
